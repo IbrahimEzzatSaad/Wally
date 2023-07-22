da9d7d03aa07d9ab7e900dd0415653038dc10aab
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-07-22 19:32:34 +0200

    Implement Repo

---
+++ app/src/main/java/com/example/wally/data/PicturesRepositoryImp.kt
@@ -0,0 +1,48 @@
+package com.example.wally.data
+
+import android.util.Log
+import com.example.wally.data.api.model.ApiPictures
+import com.example.wally.domain.repository.PicturesRepository
+import com.example.wally.domain.usecases.NetworkException
+import com.example.wally.data.api.PicturesApi
+import com.example.wally.data.api.model.PicturesItem
+import com.example.wally.data.cache.Cache
+import com.example.wally.data.cache.model.CachedPicture
+import com.example.wally.data.cache.model.toDomain
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class PicturesRepositoryImp @Inject constructor(
+    private val cache: Cache,
+    private val api: PicturesApi
+) : PicturesRepository {
+
+    override suspend fun requestPictures(page: String): List<PicturesItem> {
+
+        try {
+            val results: ApiPictures = api.getPictures(page)
+            return results.toList()
+        } catch (exception: HttpException) {
+            throw NetworkException(exception.message ?: "Code ${exception.code()}")
+        }
+    }
+
+    override suspend fun storePictures(pictures: List<PicturesItem>) {
+        cache.deleteAll()
+        pictures.forEach { picture ->
+            Log.i("ListInformation:", picture.toString())
+            cache.storePictures(CachedPicture.fromDomain(picture))
+        }
+    }
+
+    override fun getPictures(): Flow<List<PicturesItem>> {
+        return cache.getPictures()
+            .distinctUntilChanged() // ensures only events with new information get to the subscriber.
+            .map { articlesList ->
+                articlesList.map { it.toDomain() }
+            }
+    }
+}
