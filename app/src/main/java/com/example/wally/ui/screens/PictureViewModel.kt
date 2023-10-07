24035257a38e69e15b7ed4a288d139ad3f9ebc70
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-10-07 04:18:15 +0200

    Add UiState Flow to ViewModel & Retry Function

--- app/src/main/java/com/example/wally/ui/screens/PictureViewModel.kt
+++ app/src/main/java/com/example/wally/ui/screens/PictureViewModel.kt
@@ -8,6 +8,7 @@ import com.example.wally.data.api.model.PicturesItem
 import com.example.wally.domain.usecases.FetchFeatured
 import com.example.wally.domain.usecases.GetFeatured
 import com.example.wally.domain.usecases.GetPictures
+import com.example.wally.utils.UiState
 import com.organization.nytimes.utils.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
@@ -19,18 +20,21 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
-class PictureViewModel  @Inject constructor(
+class PictureViewModel @Inject constructor(
     private val getPictures: GetPictures,
     private val getFeatured: GetFeatured,
     private val fetchFeatured: FetchFeatured,
     private val mainDispatcher: CoroutineDispatcher = Dispatchers.IO
-) : ViewModel(){
+) : ViewModel() {
 
-    private val _pictures : MutableStateFlow<PagingData<PicturesItem>> = MutableStateFlow(value = PagingData.empty())
+    private val _pictures: MutableStateFlow<PagingData<PicturesItem>> =
+        MutableStateFlow(value = PagingData.empty())
     val pictures: StateFlow<PagingData<PicturesItem>> =
         _pictures.asStateFlow()
 
@@ -39,20 +43,30 @@ class PictureViewModel  @Inject constructor(
     val featured: StateFlow<List<PicturesItem>?> =
         _featured.asStateFlow()
 
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
 
     init {
         viewModelScope.launch(mainDispatcher) {
-            fetchFeatured()
-            subscribeToFeaturedUpdates()
-            subscribeToPicturesUpdates()
+            try {
+                onNewUiState(true, "")
+                fetchFeatured()
+                subscribeToFeaturedUpdates()
+                subscribeToPicturesUpdates()
+
+            } catch (e: IOException) {
+                e.message?.let { onNewUiState(false, it) }
+                subscribeToFeaturedUpdates()
+                subscribeToPicturesUpdates()
+
+            }
         }
     }
 
 
-
     private fun subscribeToPicturesUpdates() {
         viewModelScope.launch(mainDispatcher) {
-
             getPictures().distinctUntilChanged().cachedIn(viewModelScope).collect {
                 onNewPicturesList(it)
             }
@@ -61,8 +75,7 @@ class PictureViewModel  @Inject constructor(
 
     private fun subscribeToFeaturedUpdates() {
         viewModelScope.launch(mainDispatcher) {
-
-            getFeatured().collect{
+            getFeatured().distinctUntilChanged().collect {
                 onNewFeaturedList(it)
             }
         }
@@ -70,13 +83,36 @@ class PictureViewModel  @Inject constructor(
 
 
     private suspend fun onNewPicturesList(pictures: PagingData<PicturesItem>) {
-        Logger.d("Got more Pictures $pictures")
         _pictures.emit(pictures)
     }
 
     private suspend fun onNewFeaturedList(pictures: List<PicturesItem>) {
-        Logger.d("Got more Featured $pictures")
         _featured.emit(pictures)
+
+        if (pictures.isNotEmpty())
+            onNewUiState(false, "")
+        else
+            onNewUiState(false, uiState.value.errorMessage)
+
     }
 
+    private fun onNewUiState(loading: Boolean, error: String) {
+        if (_uiState.value != UiState(loading, error)) {
+            _uiState.update { currentUiState ->
+                currentUiState.copy(isLoading = loading, errorMessage = error)
+            }
+        }
+    }
+
+    fun retry() {
+        viewModelScope.launch(mainDispatcher) {
+            try {
+                onNewUiState(true, uiState.value.errorMessage)
+                fetchFeatured()
+            } catch (e: IOException) {
+                e.message?.let { onNewUiState(loading = false, error = it) }
+            }
+
+        }
+    }
 }
---
+++ app/src/main/java/com/example/wally/utils/UiState.kt
@@ -0,0 +1,6 @@
+package com.example.wally.utils
+
+data class UiState(
+    val isLoading: Boolean = false,
+    val errorMessage: String = ""
+)
