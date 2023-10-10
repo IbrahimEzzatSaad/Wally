a8d13792a2353153cf65c0529228746d1ba33307
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-10-10 07:15:38 +0200

    Add heart favorite button to PictureItem.kt.

--- app/src/main/java/com/example/wally/ui/component/PictureItem.kt
+++ app/src/main/java/com/example/wally/ui/component/PictureItem.kt
@@ -1,16 +1,26 @@
 package com.example.wally.ui.component
 
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -20,8 +30,11 @@ import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
+import com.example.wally.Duration
+import com.example.wally.R
 import com.example.wally.data.api.model.PicturesItem
 import com.example.wally.ui.screens.OnPictureItemClicked
+import com.exyte.animatednavbar.items.dropletbutton.DropletButton
 
 @Composable
 fun PictureItem(
@@ -44,6 +57,7 @@ fun PictureItem(
             defaultElevation = 8.dp
         )
     ) {
+        var favorite by remember { mutableStateOf(false) }
 
         val painter = rememberAsyncImagePainter(item.urls.small)
 
@@ -52,14 +66,39 @@ fun PictureItem(
             label = ""
         )
 
-        Image(
-            painter = painter,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxSize()
-                .alpha(transition),
-            contentDescription = "custom transition based on painter state"
-        )
+        Box {
+            Image(
+                painter = painter,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(transition),
+                contentDescription = "custom transition based on painter state"
+            )
 
+            Card(modifier = Modifier.align(Alignment.BottomEnd)
+                .padding(6.dp)
+                .height(30.dp)
+                .width(30.dp)
+                .alpha(if(favorite) 1f else 0.4f),
+                shape = CircleShape,
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 8.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                ),){
+                DropletButton(
+                    modifier = Modifier.fillMaxSize().padding(5.dp),
+                    isSelected = favorite,
+                    onClick = {
+                        favorite = !favorite
+                    },
+                    icon = R.drawable.heart,
+                    dropletColor = Color.Red,
+                    animationSpec = tween(durationMillis = Duration, easing = LinearEasing)
+                )
+            }
+        }
     }
 }
