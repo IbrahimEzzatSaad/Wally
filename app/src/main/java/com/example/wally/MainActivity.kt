006de47134a6396dede3da06bf36eff32cd741ea
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-10-10 07:14:53 +0200

    Add bottom navigation bar.

--- app/src/main/java/com/example/wally/MainActivity.kt
+++ app/src/main/java/com/example/wally/MainActivity.kt
@@ -1,28 +1,28 @@
 package com.example.wally
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +32,14 @@ import androidx.navigation.compose.rememberNavController
 import com.example.wally.ui.navigation.SplashScreen
 import com.example.wally.ui.navigation.AppNavHost
 import com.example.wally.ui.navigation.appScreens
+import com.example.wally.ui.navigation.dropletButtons
+import com.example.wally.ui.theme.BluishGray
 import com.example.wally.ui.theme.WallyTheme
+import com.exyte.animatednavbar.AnimatedNavigationBar
+import com.exyte.animatednavbar.animation.balltrajectory.Parabolic
+import com.exyte.animatednavbar.animation.indendshape.Height
+import com.exyte.animatednavbar.animation.indendshape.shapeCornerRadius
+import com.exyte.animatednavbar.items.dropletbutton.DropletButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -46,7 +53,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.surface
                 ) {
-                    myApp()
+                    MyApp()
                 }
             }
         }
@@ -56,25 +63,61 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 @Preview
-private fun myApp() {
+private fun MyApp() {
     val navController = rememberNavController()
     val currentBackStack by navController.currentBackStackEntryAsState()
     val currentDestination = currentBackStack?.destination
-    /*val currentScreen =
-        appScreens.find { it.route == currentDestination?.route } ?: AllPictures*/
+    val currentScreen =
+        appScreens.find { it.route == currentDestination?.route } ?: SplashScreen
+    var selectedIndex by remember { mutableStateOf(0) }
 
+  
     Scaffold(
 
         modifier = Modifier.semantics {
             testTagsAsResourceId = true
         },
+        bottomBar = {
+            if(currentScreen != SplashScreen){  AnimatedNavigationBar(modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 6.dp)
+                .height(60.dp),
+                selectedIndex = selectedIndex,
+                ballColor = White,
+                barColor = BluishGray,
+                cornerRadius = shapeCornerRadius(25.dp),
+                ballAnimation = Parabolic(tween(Duration, easing = LinearOutSlowInEasing)),
+                indentAnimation = Height(
+                    indentWidth = 56.dp,
+                    indentHeight = 15.dp,
+                    animationSpec = tween(
+                        DoubleDuration,
+                        easing = { OvershootInterpolator().getInterpolation(it) })
+                )
+            ) {
+                dropletButtons.forEachIndexed { index, it ->
+                    DropletButton(modifier = Modifier.fillMaxSize(),
+                        isSelected = selectedIndex == index,
+                        onClick = { selectedIndex = index
+                        },
+                        icon = it.icon,
+                        dropletColor = White,
+                        animationSpec = tween(durationMillis = Duration, easing = LinearEasing)
+                    )
+                }
 
-        content = { innerPadding ->
-                AppNavHost(
+            }}
+        },
+                content = { innerPadding ->
+                    Log.i("Padding", "is = $innerPadding")
 
+                    AppNavHost(
                     navController = navController,
-                    modifier = Modifier.padding(innerPadding)
+                    modifier = Modifier
                 )
 
         })
 }
+
+
+const val Duration = 500
+const val DoubleDuration = 1000
--- app/src/main/java/com/example/wally/ui/navigation/AppDestinations.kt
+++ app/src/main/java/com/example/wally/ui/navigation/AppDestinations.kt
@@ -1,5 +1,11 @@
 package com.example.wally.ui.navigation
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Stable
+import com.example.wally.R
+
 interface AppDestinations {
     val title: String
     val route: String
@@ -7,7 +13,22 @@ interface AppDestinations {
 
 object HomeScreen : AppDestinations {
     override val title = "HomeScreen"
-    override val route = "pictures"
+    override val route = "home"
+}
+
+object SearchScreen : AppDestinations {
+    override val title = "SearchScreen"
+    override val route = "search"
+}
+
+object FavoriteScreen : AppDestinations {
+    override val title = "FavoriteScreen"
+    override val route = "favorite"
+}
+
+object CategoriesScreen : AppDestinations {
+    override val title = "CategoriesScreen"
+    override val route = "categories"
 }
 
 object SplashScreen : AppDestinations {
@@ -21,3 +42,38 @@ object PictureDetails : AppDestinations {
     const val pictureKey = "picture_argument"
 }
 
+
+@Stable
+data class Item(
+    @DrawableRes val icon: Int,
+    var isSelected: Boolean,
+    @StringRes val description: Int,
+    val destination : AppDestinations
+)
+
+val dropletButtons = listOf(
+    Item(
+        icon = R.drawable.home,
+        isSelected = false,
+        description = R.string.home,
+        destination = HomeScreen
+    ),
+    Item(
+        icon = R.drawable.categories,
+        isSelected = false,
+        description = R.string.categories,
+        destination = CategoriesScreen
+    ),
+    Item(
+        icon = R.drawable.search,
+        isSelected = false,
+        description = R.string.search,
+        destination = SearchScreen
+    ),
+    Item(
+        icon = R.drawable.heart,
+        isSelected = false,
+        description = R.string.favorite,
+        destination = FavoriteScreen
+    ),
+)
