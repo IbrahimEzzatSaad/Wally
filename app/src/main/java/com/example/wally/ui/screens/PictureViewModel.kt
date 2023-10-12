1834e3cc51900d2cf40e00be53d8896bc9510dd6
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-10-12 16:22:16 +0200

    Remove favorite list flow from ViewModel & add UpdateFavorites function.

--- app/src/main/java/com/example/wally/ui/screens/PictureViewModel.kt
+++ app/src/main/java/com/example/wally/ui/screens/PictureViewModel.kt
@@ -6,20 +6,18 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.wally.data.api.model.PicturesItem
 import com.example.wally.domain.usecases.FetchFeatured
+import com.example.wally.domain.usecases.GetFavorite
 import com.example.wally.domain.usecases.GetFeatured
 import com.example.wally.domain.usecases.GetPictures
+import com.example.wally.domain.usecases.UpdateFavorite
 import com.example.wally.utils.UiState
-import com.organization.nytimes.utils.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.IOException
@@ -30,6 +28,7 @@ class PictureViewModel @Inject constructor(
     private val getPictures: GetPictures,
     private val getFeatured: GetFeatured,
     private val fetchFeatured: FetchFeatured,
+    private val updateFavorite: UpdateFavorite,
     private val mainDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
 
@@ -42,6 +41,8 @@ class PictureViewModel @Inject constructor(
     private val _featured = MutableStateFlow<List<PicturesItem>?>(null)
     val featured: StateFlow<List<PicturesItem>?> =
         _featured.asStateFlow()
+
+
 
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
@@ -73,17 +74,16 @@ class PictureViewModel @Inject constructor(
         }
     }
 
+    private suspend fun onNewPicturesList(pictures: PagingData<PicturesItem>) {
+        _pictures.emit(pictures)
+    }
+
     private fun subscribeToFeaturedUpdates() {
         viewModelScope.launch(mainDispatcher) {
             getFeatured().distinctUntilChanged().collect {
                 onNewFeaturedList(it)
             }
         }
-    }
-
-
-    private suspend fun onNewPicturesList(pictures: PagingData<PicturesItem>) {
-        _pictures.emit(pictures)
     }
 
     private suspend fun onNewFeaturedList(pictures: List<PicturesItem>) {
@@ -95,6 +95,14 @@ class PictureViewModel @Inject constructor(
             onNewUiState(false, uiState.value.errorMessage)
 
     }
+
+
+    fun updateFavorites(id : String){
+        viewModelScope.launch(mainDispatcher) {
+            updateFavorite(id)
+        }
+    }
+
 
     private fun onNewUiState(loading: Boolean, error: String) {
         if (_uiState.value != UiState(loading, error)) {
