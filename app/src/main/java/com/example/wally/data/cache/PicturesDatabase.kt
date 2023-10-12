39be4713af867d38b4fdcdf34d6288dbf97de7b5
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-10-12 16:04:13 +0200

    Add entities to the Database.

--- app/src/main/java/com/example/wally/data/cache/PicturesDatabase.kt
+++ app/src/main/java/com/example/wally/data/cache/PicturesDatabase.kt
@@ -3,17 +3,30 @@ package com.example.wally.data.cache
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.wally.data.cache.daos.FavoriteDao
+import com.example.wally.data.cache.daos.FeaturedDao
+import com.example.wally.data.cache.daos.PageKeyDao
 import com.example.wally.data.cache.daos.PicturesDao
+import com.example.wally.data.cache.model.CachedFavoritePicture
+import com.example.wally.data.cache.model.CachedFeaturedPicture
 import com.example.wally.data.cache.model.CachedPicture
 import com.example.wally.data.cache.model.Converter
+import com.example.wally.data.cache.model.PageKeyEntity
 
 /**
  * Database class with a singleton INSTANCE object.
  */
-@Database(entities = [CachedPicture::class], version = 1)
+@Database(entities = [CachedPicture::class, CachedFavoritePicture::class, CachedFeaturedPicture::class, PageKeyEntity::class], version = 1)
 @TypeConverters(Converter::class)
 abstract class PicturesDatabase : RoomDatabase() {
 
     abstract fun picturesDao(): PicturesDao
+
+    abstract fun favoritesDao(): FavoriteDao
+
+    abstract fun featuredDao(): FeaturedDao
+
+    abstract fun pageKeyDao(): PageKeyDao
+
 
 }
