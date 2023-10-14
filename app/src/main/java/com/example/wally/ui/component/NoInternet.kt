c5c8a3eaca0dab18e538a665e990ff6a99fca846
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-10-14 18:44:08 +0200

    Edit Image NoInternet height to leave a space for Navigation

--- app/src/main/java/com/example/wally/ui/component/NoInternet.kt
+++ app/src/main/java/com/example/wally/ui/component/NoInternet.kt
@@ -3,9 +3,11 @@ package com.example.wally.ui.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -44,7 +46,7 @@ fun NoInternet(retry: OnRetryClicked, enabled: Boolean) {
                 contentDescription = "No connection",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(550.dp)
+                    .height(510.dp)
                     .align(Alignment.Center)
             )
         }
@@ -59,5 +61,7 @@ fun NoInternet(retry: OnRetryClicked, enabled: Boolean) {
         ) {
             Text("Try Again...", Modifier.padding(5.dp))
         }
+
     }
+
 }
