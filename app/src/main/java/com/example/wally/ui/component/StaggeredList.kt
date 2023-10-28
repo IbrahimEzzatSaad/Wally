ddcf45ed4e0bb50dab28e399df6a82a1d17fa764
Ibrahim Ezzat <hemaezzat2715@gmail.com>
2023-10-28 14:13:56 +0200

    Add refresh button in StaggeredList

--- app/src/main/java/com/example/wally/ui/component/StaggeredList.kt
+++ app/src/main/java/com/example/wally/ui/component/StaggeredList.kt
@@ -1,37 +1,45 @@
 package com.example.wally.ui.component
 
 import android.content.res.Configuration
-import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemContentType
 import androidx.paging.compose.itemKey
+import com.example.wally.R
 import com.example.wally.data.api.model.PicturesItem
-import com.example.wally.ui.screens.OnFavoriteClicked
-import com.example.wally.ui.screens.OnFeaturedItemClicked
-import com.example.wally.ui.screens.OnPictureItemClicked
-import com.example.wally.ui.screens.PICTURE_LIST_TEST_TAG
+import com.example.wally.ui.OnFavoriteClicked
+import com.example.wally.ui.OnFeaturedItemClicked
+import com.example.wally.ui.OnPictureItemClicked
+import com.example.wally.ui.screens.home.PICTURE_LIST_TEST_TAG
+import com.example.wally.ui.theme.BluishGray
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -125,8 +133,47 @@ fun StaggeredList(
             }
         }
 
+        pictures?.apply {
+            when {
+                loadState.refresh is LoadState.Error || loadState.append is LoadState.Error -> {
+                    val error = when {
+                        loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+                        else -> loadState.append as LoadState.Error
+                    }
+
+
+                    item(span = StaggeredGridItemSpan.FullLine) {
+                        Box(modifier = Modifier
+                            .wrapContentWidth()
+                            .wrapContentHeight()) {
+
+
+                            FilledIconButton(
+                                modifier = Modifier
+                                    .size(65.dp)
+                                    .padding(10.dp)
+                                    .align(Alignment.Center)
+                                    .shadow(5.dp, CircleShape),
+                                onClick = {pictures.retry()},
+                                shape = CircleShape,
+                                colors = IconButtonDefaults.filledIconButtonColors(containerColor = BluishGray)
+                            ) {
+                                Icon(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(8.dp),
+                                    painter = painterResource(id = R.drawable.refresh),
+                                    contentDescription = "refresh"
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         item(span = StaggeredGridItemSpan.FullLine) {
-            Spacer(modifier = Modifier.size(60.dp))
+            Spacer(modifier = Modifier.size(65.dp))
         }
     }
 }
